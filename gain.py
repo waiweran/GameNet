1718a7c3ea0ea9f0ae26994e71b81063fcf0ac8e
Nathaniel Brooke <neb17@duke.edu>
2019-03-27 16:42:20 -0400

    Updated gain to be class based

--- gain.py
+++ gain.py
@@ -8,24 +8,26 @@ from tensorflow import keras
 import numpy as np
 import json
 
-def load_model():
-	model = keras.models.load_model('checkpoints/gain.h5')
-	return model
+class GainModel:
 
-def run_game(model, input):
-	indict = json.loads(instring)
-	netin = (np.expand_dims(indict['GainChoice'], 0))
-	output = model.predict(netin)
-	output = output.tolist()[0]
-	return json.dumps(output)
+	def __init__(self):
+		self.model = keras.models.load_model('checkpoints/gain.h5')
+
+	def run_game(self, input):
+		indict = json.loads(instring)
+		netin = (np.expand_dims(indict['GainChoice'], 0))
+		output = self.model.predict(netin)
+		output = output.tolist()[0]
+		return json.dumps(output)
+
 
 if __name__ == '__main__':
-	model = load_model()
-	print(model.input_shape)
-	model.summary()
+	gain = GainModel()
+	print(gain.model.input_shape)
+	gain.model.summary()
 	while True:
 		instring = input()
-		print(run_game(model, instring))
+		print(run_game(instring))
 
 
 
