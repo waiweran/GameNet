c9fd409940fc8bd93de827f7417a9f7efa658442
Nathaniel Brooke <neb17@duke.edu>
2019-03-27 19:55:47 -0400

    Updated app

--- flask-app.py
+++ flask-app.py
@@ -10,11 +10,12 @@ def main_page():
 
 @app.route('/predict/', methods=['GET', 'POST'])
 def predict_page():
-    if request.is_json:
-        output = model.run_game(str(request.get_json()))
+    instring = request.form['input']
+    if instring.startswith('{'):
+        output = model.run_game(instring)
         return render_template('predict.html', output=output)
+    return render_template('predict.html', output="Incorrect Input")
 
-    return redirect('/')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
