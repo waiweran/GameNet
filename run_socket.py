2bbc289a7080cb24253dade330f097780d7f30f3
Nathaniel Brooke <neb17@duke.edu>
2019-03-27 16:58:20 -0400

    Updated socket method

--- run_socket.py
+++ run_socket.py
@@ -3,19 +3,9 @@ import _thread
 import json
 import gain
 
-def run_game(model, connection):
-	total = 0
-	while(True):
-		instring = connection.recv(2048).decode()
-		if instring.startswith('{'):
-			print("Received " + str(total))
-			total += 1
-			output = model.run_game(instring)
-			connection.send((output + '\n').encode())
-
 
 try:
-
+	
 	# Initialize Network
 	model = gain.GainModel()
 
@@ -26,10 +16,14 @@ try:
 	s.listen(5)	 
 	print('socket is listening on port {port}'.format(port=port))			
 
-	# Accept connections
+	# Accept connections and run moves
 	while True:
 		conn, addr = s.accept()	 
-		print('Got connection from {addr}'.format(addr=addr))
-		run_game(model, conn)
+		instring = connection.recv(2048).decode()
+		if instring.startswith('{'):
+			output = model.run_game(instring)
+			connection.send((output + '\n').encode())
+		conn.close()
+
 finally: 
 	s.close()
