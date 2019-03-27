9821fc2827ca79a7327936f12748d8a5669797a9
VCM admin account <vcm@vcm-402.vm.duke.edu>
2019-03-27 19:51:07 -0400

    Updates to flask app

--- flask-app.py
+++ flask-app.py
@@ -10,11 +10,11 @@ def main_page():
 
 @app.route('/predict/', methods=['GET', 'POST'])
 def predict_page():
-    if request.is_json():
+    if request.is_json:
         output = model.run_game(str(request.get_json()))
         return render_template('predict.html', output=output)
 
     return redirect('/')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
